afd1576c0c36db15fd24a15bf927266d623662d4
Barashkov <nebunohu@gmail.com>
2021-11-12 16:53:42 +0300

    API added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -9,23 +9,50 @@ import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import AppStyles from './App.module.css';
 
 // Data
-import { data } from '../../utils/data';
+//import { data } from '../../utils/data';
+
 
 function App() {
+  const dataURL = 'https://norma.nomoreparties.space/api/ingredients';
   const [state, setState] = React.useState(
     {
       burger: {
-        bun: data[0],
-        ingredients: [...data],
-      }
+        bun: {},
+        ingredients: [],
+      }, 
+      data: []
     }
   );
+
+  React.useEffect(() => {
+    const getIngredientsData = async () => {
+      let headers = new Headers({ "content-type": "application/json" });
+      try {
+        const res = await fetch(dataURL, { method: "GET", mode: "cors", headers});
+        if(res.ok) {
+          const data = await res.json();
+          setState({
+            ...state,
+            data: data.data,
+          });
+        } else {
+          throw new Error('Fetch error'); 
+        }
+        
+      } catch (e) {
+        console.log(e);
+      }
+      
+    }
+
+    getIngredientsData();
+});
 
   return (
     <>
       <AppHeader />
       <div className={AppStyles.BurgerWrapper}>
-        <BurgerIngredients data={data} />
+        <BurgerIngredients data={state.data} />
         <BurgerConstructor burger={state.burger} /> 
       </div>
     </>
