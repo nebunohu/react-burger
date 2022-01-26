345a19a6de66328e8206a58cade7bdd94a2869a8
Barashkov <nebunohu@gmail.com>
2022-01-26 17:13:45 +0300

    intermediateI commit

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -6,36 +6,37 @@ import feedPageStyles from './feed.module.css';
 
 const FeedPage: FC = () => {
   return (
-    <div className={`${feedPageStyles.wrapper}`}>
+    <div className={`${feedPageStyles.container}`}>
       <div className={`${feedPageStyles.feedHeader} text text_type_main-large`}>Лента заказов</div>
       <div className={`${feedPageStyles.wrapper}`}>
-        <section className={`${feedPageStyles.ordersFeedWrapper}`}>
+        <section className={`${feedPageStyles.ordersFeedWrapper} mr-15`}>
+          <FeedOrder />
           <FeedOrder />
         </section>  
-        <section>
-          <div>
-            <div>
-              <div>
-                ГотовыЖ
+        <section className={`${feedPageStyles.ordersInfoWrapper}`}>
+          <div className="mb-15">
+            <div className={`${feedPageStyles.readyOrdersWrapper} mr-6`}>
+              <div className={`text text_type_main-medium mb-6`}>
+                Готовы:
               </div>
               <div>
                 orders
               </div>
             </div>
-            <div>
-              <div>
-                В рвботе:
+            <div className={`${feedPageStyles.inProgressOrdersWrapper}`}>
+              <div className="text text_type_main-medium  mb-6">
+                В работе:
               </div>
-              <div>
+              <div >
                 orders                
               </div>
             </div>
           </div>
-          <div>
-            Выполнено за всё время:
+          <div className="text text_type_main-medium mb-15">
+            Выполнено за всё время: <br/> <span className={`${feedPageStyles.number} text text_type_digits-large`}>0000</span>
           </div>
-          <div>
-            Выполнено за сегодня:
+          <div className="text text_type_main-medium">
+            Выполнено за сегодня: <br/> <span className={`${feedPageStyles.number} text text_type_digits-large`}>0000</span>
           </div>
         </section>
       </div>
---
+++ src/services/actions/order-actions.ts
@@ -0,0 +1,7 @@
+
+export const getOrders = () => {
+  return async (dispatch) => {
+      
+  }
+
+}
